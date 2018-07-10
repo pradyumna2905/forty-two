7f5cd1a31679875c5f2dd9a584b0a426c79629be
Pradyumna <pradyumna2905@gmail.com>
2018-07-09 23:28:28 -0700

    Registration page.

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 gem 'jquery-rails'
 gem 'bootstrap', '~> 4.1.1'
+
+# HAML
+gem 'hamlit'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  devise_scope :user do
+    unauthenticated :user do
+      root to: 'devise/registrations#new'
+    end
+
+    authenticated :user do
+      # root to: '?', as: :authenticated_root
+    end
+  end
 end
