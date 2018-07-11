0a000f09f15239f84588e3c0006a8ba7c2585048
Pradyumna <pradyumna2905@gmail.com>
2018-07-10 20:53:32 -0700

    Adds Entry model.

---
+++ db/migrate/20180711035258_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries, id: :uuid do |t|
+      t.string :word
+      t.text :meaning
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_013220) do
+ActiveRecord::Schema.define(version: 2018_07_11_035258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2018_07_11_013220) do
     t.text "response"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "word"
+    t.text "meaning"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
